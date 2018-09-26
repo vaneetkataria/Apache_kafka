72ac9bdbfe5994bbc15c9515ba4ad1bef5a08b94
vaneet <vaneetkataria54@gmail.com>
2018-09-26 16:22:53 +0530

    Added Basic setup code for Kafka Concepts demo .

---
+++ kafka_concepts_demo/src/main/java/com/katariasoft/technologies/kafka/producer/NativeKafkaProducer.java
@@ -0,0 +1,12 @@
+package com.katariasoft.technologies.kafka.producer;
+
+public class NativeKafkaProducer {
+
+	public NativeKafkaProducer() {
+	}
+
+	public static void main(String args[]) {
+
+	}
+
+}
